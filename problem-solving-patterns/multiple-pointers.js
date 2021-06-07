059d1473c7224d2fccf315ca32f02dc37cd9f2b8
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-06-07 15:21:20 -0700

    additional solution for counting unique values

--- problem-solving-patterns/multiple-pointers.js
+++ problem-solving-patterns/multiple-pointers.js
@@ -28,4 +28,20 @@ function countUniqueValues(arr){
   return i + 1;
 }
 
-console.log(countUniqueValues([-2,-1,-1,0,1]));
+function countUniqueValuesTwo(arr){
+  if(arr.length === 0){
+    return 0;
+  }
+
+  let i = 0;
+  for(let j = 1; j < arr.length; j++){
+    if(arr[i] !== arr[j]){
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+// console.log(countUniqueValues([-2,-1,-1,0,1]));
+console.log(countUniqueValuesTwo([-2,-1,-1,0,1]));
