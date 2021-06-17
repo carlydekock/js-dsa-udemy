3769f50a0e86811ad4b7ceb576333d162d9e9097
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-06-16 22:03:05 -0700

    linear search challenge complete

---
+++ searching-algorithms/linear-search.js
@@ -0,0 +1,16 @@
+//Linear Search Pseudocode
+//This function accepts an array and a value
+//Loop through the array and check if the current array element is equal to the value
+//If it is, return the index at which the element is found
+//If the value is never found, return -1
+
+function linearSearch(arr, value){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === value){
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch([1,2,6,9,15,64,3,17], 9));
