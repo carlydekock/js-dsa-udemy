15a33b0dd3c49f162de60061ebba6b76a60fedd1
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-06-21 11:34:58 -0700

    pseudocode added for both optional challenges

---
+++ optional-challenges/longest-substring.js
@@ -0,0 +1,5 @@
+//Write a function called findLongestSubsring which accepts a string and returns the length of the longest substring with all distinct characters.
+//Ex: findLongestSubstring('') // 0
+//findLongestSubstring('rithmschool') // 7
+//findLongestSubstring('bbbb') // 1
+
