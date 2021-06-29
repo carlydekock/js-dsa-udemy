b88233388537cd104f4d6164a96b963d36b53b59
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-06-28 17:28:10 -0700

    bubble sort complete

---
+++ sorting-algorithms/bubble-sort.js
@@ -0,0 +1,21 @@
+//Bubble Sort Pseudocode
+//Start looping with a variable called i at the end of the array towards the beginning
+//Start an inner loop with a variable j fromt he beginning until i - 1
+//If arr[j] is greater than arr[j+1], swap those two values
+//Return the sorted array
+
+function bubbleSort(arr){
+  for(let i = arr.length; i > 0; i--){
+    for(let j = 0; j < i - 1; j++){
+      console.log(arr, arr[j], arr[j+1], i-1)
+      if (arr[j] > arr[j+1]){
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([2,3,7,4,6]));
