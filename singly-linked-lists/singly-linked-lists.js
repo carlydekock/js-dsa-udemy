b4e58763dc70d4a5ae5ae2a1661015c592a064c4
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-06 12:16:03 -0700

    push method complete

--- singly-linked-lists/singly-linked-lists.js
+++ singly-linked-lists/singly-linked-lists.js
@@ -10,8 +10,32 @@ class SinglyLinkedList {
   constructor(){
     this.head = null;
     this.tail = null;
-    this.legnth = 0;
+    this.length = 0;
+  }
+
+  //Push method: take a piece of data, should create and assign as a new node to the list
+  //Takes in a value, create a new node using the val
+  //If there is no head, set the head and the tail to be the new node
+  //Otherwise, set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+  //Increment the length by one
+  //Return the linked list
+
+  push(val){
+    let newNode = new Node(val);
+    if(!this.head){
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
   }
 }
 
 let list = new SinglyLinkedList();
+list.push('hello');
+list.push('goodbye');
+list.push(10);
+console.log(list);
