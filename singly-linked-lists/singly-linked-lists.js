81b0c89ab180046d4d3e9f0f8b16678d58835ca3
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-07 15:34:54 -0700

    unshift method complete

--- singly-linked-lists/singly-linked-lists.js
+++ singly-linked-lists/singly-linked-lists.js
@@ -81,6 +81,28 @@ class SinglyLinkedList {
     }
     return oldHead;
   }
+
+  //Unshift: Adding a new node to the beginning of the linked list
+  //Function should accept a value
+  //Create a new node using the value passed to the function
+  //If there is no head property on the list, set the head and tail to be the newly created node
+  //Otherwise, set the newly created node's next property to be the current head property on the list
+  //Set the head property on the list to be that newly created node
+  //Increment the length of the list by 1
+  //Return the linked list
+
+  unshift(val){
+    let newNode = new Node(val);
+    if(!this.head){
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
@@ -88,5 +110,4 @@ list.push(5);
 list.push(10);
 list.push(15);
 console.log(list);
-console.log(list.shift());
-console.log(list);
+console.log(list.unshift(3));
