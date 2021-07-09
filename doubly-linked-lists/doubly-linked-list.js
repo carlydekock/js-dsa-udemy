cd12cf64d1f1a413cfec397a12d592203837e7f5
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-09 16:30:59 -0700

    insert method complete

--- doubly-linked-lists/doubly-linked-list.js
+++ doubly-linked-lists/doubly-linked-list.js
@@ -155,6 +155,38 @@ class DoublyLinkedList {
     }
     return false;
   }
+
+  //Insert: adding a node in a list by a certain position
+  //If the index is less than 0 or greater than or equal to the length return false
+  //If the index is 0, unshift
+  //If the index is the same as the length, push
+  //Use the get method to access the index -1
+  //Set the next and prev properties on the correct nodes to link everything together
+  //Increment the length
+  //Return true
+
+  insert(index, value){
+    if(index < 0 || index >= this.length){
+      return false;
+    }
+    if(index === 0){
+      return !!this.unshift(value);
+    }
+    if(index === this.length){
+      return !!this.push(value);
+    }
+    let previous = this.get(index-1);
+    let newNode = new Node(value);
+    let after = previous.next;
+    previous.next = newNode;
+    newNode.prev = previous;
+    newNode.next = after;
+    after.prev = newNode;
+    this.length++;
+    return true;
+  }
+
+
 }
 
 let list = new DoublyLinkedList();
@@ -162,5 +194,5 @@ list.push(5);
 list.push(10);
 list.push(15);
 list.push(20);
-console.log(list.set(-10, 30));
+console.log(list.insert(0, "first"));
 console.log(list);
