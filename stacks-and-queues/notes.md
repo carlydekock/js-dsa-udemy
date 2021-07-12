302600089e98760c0a2c36a473208ca18035fd27
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-12 11:41:13 -0700

    queues methods and notes complete

--- stacks-and-queues/notes.md
+++ stacks-and-queues/notes.md
@@ -1,6 +1,6 @@
 # Stacks and Queues
 
-## Stacks
+## **Stacks**
 
 - Abstract data structure or concept, collection of data that needs to abide by a LIFO principle (last in first out).
 - The last element added to a stack will be the first one removed. (Example: stack of books, or stack of dishes, you'll remove the one just added from the top)
@@ -8,7 +8,7 @@
 - Where are stacks used: managing function invocations, undo/redo, routing (the history object) is treated like a stack
 - Not a built in data structure in JavaScript like they are in other languages
 
-### Array Implementation
+### Array Implementation - Stacks
 
 - Using push and pop on an array functions like a stack
 - Using unshift and shift on an array functions like a stack as well
@@ -17,6 +17,25 @@
 - Adding/removing from the same direction is the key
 
 ### Big O of Stacks
+
+- Insertion: O(1)
+- Removal: O(1)
+- Searching: O(N)
+- Access: O(N)
+
+## **Queues**
+
+- Abstract data structure or concept, collection of data that needs to abide by FIFO principle (first in first out)
+- Where are stacks used: games, background tasks, uploading/downloading resources, printing/task processing
+- Can be implemented with an array, or with our own queue class
+
+### Array Implementation - Queues
+
+- Using push and shift on an array functions like a queue
+- Using unshift and pop on an array functions like a queue as well
+- Again, this is not great for Big O complexity because everything is re-indexed with shift/unshift
+
+### Big O of Queues
 
 - Insertion: O(1)
 - Removal: O(1)
--- stacks-and-queues/stacks-and-queues.js
+++ stacks-and-queues/stacks-and-queues.js
@@ -65,3 +65,61 @@ class Stack {
 // console.log(stack.pop());
 // console.log(stack.pop());
 
+
+//Queue Implementation with Linked List
+//Same Node class/constructor as above for Stacks
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  //Enqueue: Add to the end
+  //Function accepts a value
+  //Create a new node using that value passed to the function
+  //If there are no nodes in the queue, set this node to be the first and last property
+  //Otherwise, set the next property on the current last to be that node, and then set the last property of teh queue to be that node
+  //Increment the size of the queue by 1 and return the size
+
+  enqueue(val){
+    let newNode = new Node(val);
+    if(!this.first){
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this.size; //This could be return ++this.size;
+  }
+
+  //Dequeue: Return the first thing that was added in
+  //If there is no first property, just return null
+  //Store the first property in a variable
+  //See if the first is the same as the last (check if there is only 1 node). If so, set the first and last to be null
+  //If there is more than 1 node, set the first property to be the next property of first
+  //Decrement the size by 1, return the value of the node dequeued
+
+  dequeue(){
+    if(!this.first) return null;
+    let temp = this.first;
+    if(this.first === this.last){
+      this.last = null;
+    }
+    this.first = temp.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+// let q = new Queue();
+// console.log(q.enqueue("First"));
+// console.log(q.enqueue("Second"));
+// console.log(q.enqueue("Third"));
+// console.log(q.dequeue());
+// console.log(q.dequeue());
+// console.log(q.dequeue());
+// console.log(q.dequeue());
