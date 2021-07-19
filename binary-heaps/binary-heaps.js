f6d22688fb1abc137e3774efac996b62e67bcae7
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-19 10:34:07 -0700

    added classes for priority queues

--- binary-heaps/binary-heaps.js
+++ binary-heaps/binary-heaps.js
@@ -103,7 +103,99 @@ class MaxBinaryHeap {
   }
 }
 
-let heap = new MaxBinaryHeap();
-// console.log(heap.insertTwo(55));
-console.log(heap.extractMax())
-console.log(heap.values);
+
+//PriorityQueue (using a min binary heap, for variety)
+//Write a min binary heap - lower number means higher priority
+//Each node has a val and a priority, use the priority to build the heap
+//Enqueue method: accepts a value and priority, makes a new node, and puts it in the right spot based off of its priority
+//Dequeue: removes root element, returns it, and rearranges heap using priority
+
+class Node {
+  constructor(val, priority){
+    this.val = val;
+    this.priority = priority;
+    //In order to have true queue behavior - first in first out, could have something like below to track time
+    // this.insertTime = Date.now();
+  }
+}
+
+class PriorityQueue {
+  constructor(){
+    this.values = [];
+  }
+
+  enqueue(val, priority){
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp(){
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while(index > 0){
+      let parentIndex = Math.floor((index-1)/2);
+      let parent = this.values[parentIndex];
+      if(element.priority >= parent.priority) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+
+  dequeue(){
+    const min = this.values[0];
+    const end = this.values.pop();
+    if(this.values.length > 0){
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  sinkDown(){
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while(true){
+      let leftChildIdx = (2 * index) + 1;
+      let rightChildIdx = (2 * index) + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if(leftChildIdx < length){
+        leftChild = this.values[leftChildIdx];
+        if(leftChild.priority < element.priority){
+          swap = leftChildIdx;
+        }
+      }
+      if(rightChildIdx < length){
+        rightChild = this.values[rightChildIdx];
+        if((swap === null && rightChild.priority < element.priority) || (swap !== null && rightChild.priority < leftChild.priority)){
+          swap = rightChildIdx;
+        }
+      }
+      if(swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
+}
+
+//Priority Queue example - ER scenario
+// let ER = new PriorityQueue();
+// ER.enqueue('common cold', 5);
+// ER.enqueue('deep wound', 1);
+// ER.enqueue('high fever', 4);
+// ER.enqueue('broken arm', 2);
+// ER.enqueue('glass in foot', 3);
+// console.log(ER.dequeue());
+// console.log(ER.dequeue())
+// console.log(ER);
+
+
+// let heap = new MaxBinaryHeap();
+// // console.log(heap.insertTwo(55));
+// console.log(heap.extractMax())
+// console.log(heap.values);
