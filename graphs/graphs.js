095ea36d0fbd9a4aabecba911b763cf90c199b69
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-27 09:24:00 -0700

    remove vertex method complete

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -34,13 +34,28 @@ class Graph {
       v => v !== vertex1
     );
   }
+
+  //Remove a vertex
+  //Should loop as long as there are any other vertices in the adjacency list for that vertex
+  //Inside the loop, call removeEdge function with the vertex we are removing and any values in the adjacency list for that vertex
+  //Delete the key in the adjacency list for that vertex
+  removeVertex(vertex){
+    while(this.adjacencyList[vertex].length){
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
 
 let g = new Graph();
 g.addVertex('Seattle');
 g.addVertex('San Francisco');
 g.addVertex('Portland');
+g.addVertex('Salt Lake City');
 g.addEdge('Seattle', 'Portland');
 g.addEdge('Seattle', 'San Francisco');
-g.removeEdge('Seattle', 'Portland');
+g.addEdge('San Francisco', 'Salt Lake City');
+g.addEdge('Salt Lake City', 'Portland');
+g.removeVertex('Seattle');
 console.log(g);
