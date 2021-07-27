e91b04315eeb33f3b2d975523f26fe05ab110c4f
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-27 10:13:08 -0700

    depth first recursive traversal complete

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -46,16 +46,59 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  //Depth first traversal - recursive (explore as far down a branch as possible before "backtracking")
+  //If vertex is empty - return (this is base case)
+  //Add vertex to results list
+  //Mark vertex as visited
+  //For each neighbor in vertex's neighbors:
+    //If neighbor is not visited:
+      // recursively call DFS on neighbor
+  
+  //More detailed pseudocode
+  //Function should accept a starting node
+  //Create a list to store the end result, to be returned at the very end
+  //Create an object to store visited vertices
+  //Create a helper function which accepts a vertex
+    //The helper function should return early if the vertex is empty
+    //The helper function should place the vertex it accepts into the visited object and pus that vertex into the result array
+    //Loop over all of the values in the adjacencyList for that vertex
+    //If any of those values have not been visited, recursively invoke the helper function with that vertex
+  //Invoke the helper function with the starting vertex
+  //Return the result array
+
+  dfsRecursive(start){
+    const results = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+    
+    function dfs(vertex){
+      if(!vertex) return null;
+      visited[vertex] = true;
+      results.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if(!visited[neighbor]){
+          return dfs(neighbor);
+        }
+      });
+    }
+    dfs(start);
+    return results;
+  }
 }
 
 let g = new Graph();
-g.addVertex('Seattle');
-g.addVertex('San Francisco');
-g.addVertex('Portland');
-g.addVertex('Salt Lake City');
-g.addEdge('Seattle', 'Portland');
-g.addEdge('Seattle', 'San Francisco');
-g.addEdge('San Francisco', 'Salt Lake City');
-g.addEdge('Salt Lake City', 'Portland');
-g.removeVertex('Seattle');
-console.log(g);
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+console.log(g.dfsRecursive('A'));
