f811b65399e69fefd25f6329a625e7e331ca6aea
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-27 09:06:58 -0700

    addedge method complete

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -1,3 +1,5 @@
+//Constructor and methods for undirected graphs
+
 class Graph {
   constructor(){
     this.adjacencyList = {};
@@ -10,10 +12,21 @@ class Graph {
       this.adjacencyList[vertex] = [];
     }
   }
+
+  //Add an edge
+  //Function should accept two vertices, vertex1 and vertex2
+  //Should find in the adjacency list the key of vertex1 and push vertex2 into the array
+  //Do the same thing for vertex2
+  addEdge(vertex1, vertex2){
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
 }
 
 let g = new Graph();
 g.addVertex('Seattle');
 g.addVertex('San Francisco');
 g.addVertex('Portland');
+g.addEdge('Seattle', 'Portland');
+g.addEdge('Seattle', 'San Francisco');
 console.log(g);
