714812fd1744d3ca391fa5e4fd6867b8b7cf3d8b
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-27 09:11:46 -0700

    remove edge method complete

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -21,6 +21,19 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+
+  //Remove an edge
+  //Function should accept two vertices, vertex1 and vertex2
+  //Should reassign the key of vertex1 to be an array that does not contain vertex2
+  //Same thing for the key of vertex2
+  removeEdge(vertex1, vertex2){
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+    );
+  }
 }
 
 let g = new Graph();
@@ -29,4 +42,5 @@ g.addVertex('San Francisco');
 g.addVertex('Portland');
 g.addEdge('Seattle', 'Portland');
 g.addEdge('Seattle', 'San Francisco');
+g.removeEdge('Seattle', 'Portland');
 console.log(g);
