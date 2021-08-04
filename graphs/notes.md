614dc16faa0178230d6fe398517df6d645112281
CARLYDEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-08-03 17:08:06 -0700

    added notes on approach for dijkstra's and basic priority queue class

--- graphs/notes.md
+++ graphs/notes.md
@@ -52,3 +52,10 @@
 - Network routing: finds open shortes path for data
 - Biology: used to model the spread of viruses among humans
 - Airline tickets: finding cheapest route to your destination
+
+### The Approach
+
+1. Every time we look to visit a new node, we pick the node with the smallest known distance to visit first.
+1. Once we've moved to the node we're going to visit, we look at each of its neighbors.
+1. For each neighboring node, we calculate the distance by summing the total edges that lead to the node we're checking *from the starting node*.
+1. If the new total distance to a node is less than the previous total, we store the new shorter distance for that node.
--- graphs/weighted-graph.js
+++ graphs/weighted-graph.js
@@ -13,6 +13,26 @@ class WeightedGraph {
   }
 }
 
+//Simple priority queue for Dijkstra's algorithm
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  };
+
+  enqueue(val, priority){
+    this.values.push({val, priority});
+    this.sort();
+  };
+
+  dequeue(){
+    return this.values.shift();
+  };
+
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  };
+}
+
 let g = new WeightedGraph();
 g.addVertex('A');
 g.addVertex('B');
@@ -21,3 +41,14 @@ g.addEdge('A', 'B', 9);
 g.addEdge('A', 'C', 5);
 g.addEdge('B', 'C', 7);
 console.log(g.adjacencyList);
+
+
+let q = new PriorityQueue();
+q.enqueue("B", 3);
+q.enqueue("C", 5);
+q.enqueue("D", 2);
+q.enqueue("Q", 20);
+console.log(q.values);
+q.enqueue("P", 1.5);
+console.log(q.values);
+console.log(q.dequeue())
